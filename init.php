d9e26807bc781d311f71c6553752d8b9f9819f8c
Jan Prachař <jan.prachar@gmail.com>
2019-09-30 23:54:53 +0200

    61. ročník

--- init.php
+++ init.php
@@ -30,8 +30,8 @@ define('SITE', '');
 
 require_once('configure.php');
 
-define('AKTUALNI_ROCNIK', 60);
-define('AKTUALNI_ROK', '2018/2019');
+define('AKTUALNI_ROCNIK', 61);
+define('AKTUALNI_ROK', '2019/2020');
 
 /* Pocitadlo */
 define('INTERVAL_MEZI_NAVSTEVAMI', 60); //minimalni interval mezi navstevami v minutach
