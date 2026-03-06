96f7141ec4891f832c3d04065b41fa2e111c09dd
Jan Prachař <jan.prachar@gmail.com>
2026-03-06 15:54:15 +0100

    delší zobrazení novinky

--- init.php
+++ init.php
@@ -40,7 +40,7 @@ define('INTERVAL_ONLINE', 20); //maximalni doba necinnosti v minutach, kdy je cl
 /* Novinky */
 define('NOVINKY_WHERES', 'celost');
 define('NOVINKY_NA_STRANKU', 6);
-define('NOVINKY_INTERVAL', 150); //maximalni stari zobrazene novinky ve dnech
+define('NOVINKY_INTERVAL', 250); //maximalni stari zobrazene novinky ve dnech
 
 define('GUEST_TEST_TEXT', '2'); //kontrolni text pro prispevatele ve foru
 
