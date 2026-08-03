af7300254ef0f6afa3b9876dc8f214c55f9a9add
Jan Prachař <jan.prachar@gmail.com>
2026-08-03 22:43:25 +0200

    RSS: dekódování entit, atom:link self, lastBuildDate

--- rss.php
+++ rss.php
@@ -4,16 +4,17 @@ include_once('init.php');
 $base = rtrim(BASE_URL, '/');
 
 $strRSS = '<?xml version=\'1.0\' encoding=\'utf-8\'?>
-<rss version=\'2.0\'>
+<rss version=\'2.0\' xmlns:atom=\'http://www.w3.org/2005/Atom\'>
 	<channel>
 		<title>Fyzikální olympiáda</title>
 		<link>' . $base . '</link>
+		<atom:link href=\'' . $base . '/rss.xml\' rel=\'self\' type=\'application/rss+xml\'/>
 		<description>Fyzikální olympiáda, aktuality.</description>
 		<language>cs</language>
 		<copyright>Copyright 2004-' . date('Y') . ', Fyzikální olympiáda</copyright>
 		<webMaster>webmaster@fyzikalniolympiada.cz (Prachař, Jan)</webMaster>
-		<pubDate>' . date('r') . '</pubDate>
-		<docs>http://backend.userland.com/rss2/</docs>
+		<lastBuildDate>' . date('r') . '</lastBuildDate>
+		<docs>https://www.rssboard.org/rss-specification</docs>
 		<image>
 			<url>' . BASE_URL . 'pic/fo_logo.gif</url>
 			<title>Fyzikální olympiáda - oficiální stránky</title>
@@ -43,9 +44,10 @@ $strRSS .= '
 Header("Content-type: text/xml; charset=utf-8");
 echo $strRSS;
 
+/** HTML novinky -> prostý text bezpečný pro XML */
 function decode($text)
 {
-	$text = str_replace('&nbsp;', ' ', $text);
-	$text = str_replace('&ndash;', '-', $text);
+	$text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+	$text = str_replace("\u{a0}", ' ', $text);
 	return htmlspecialchars(strip_tags($text), ENT_XML1);
 }
